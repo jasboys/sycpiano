43c626d78d372b49abcd260859a5a87f33b5a08c
Sean Chen <kamiyo@gmail.com>
2023-08-22 22:46:28 -0500

    maybe we don't need docker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,21 @@
 /* eslint-disable no-var, strict, prefer-arrow-callback */
-'use strict';
 
 module.exports = {
     apps: [
         {
-            name: "sycpiano",
-            script: "yarn",
-            args: "start",
-            exec_mode: "fork",
-            interpreter: "/bin/bash",
+            name: 'sycpiano',
+            script: 'yarn',
+            args: 'node packages/server/build/app.js',
+            exec_mode: 'fork',
+            interpreter: '/bin/bash',
+            watch: true,
+            watch_delay: 1000,
+            exp_backoff_restart_delay: 100,
+            stop_exit_codes: [0],
             env: {
-                NODE_ENV: "production",
+                NODE_ENV: 'production',
             },
-            log_date_format: "YYYY-MM-DD HH:mm Z"
-        }
-    ]
-}
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+        },
+    ],
+};
