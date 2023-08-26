2d783daaeae08bada7d3bba23306374e4a536b61
Sean Chen <kamiyo@gmail.com>
2023-08-25 23:46:59 -0500

    grrr customer

--- packages/server/migrations/20230705055529_staging.sql
+++ packages/server/migrations/20230705055529_staging.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS "user" (
 
 CREATE TABLE IF NOT EXISTS user_product (
     id              uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    user_id         text REFERENCES customer ON DELETE CASCADE,
+    user_id         text REFERENCES "user" ON DELETE CASCADE,
     product_id      text REFERENCES product ON DELETE CASCADE
 );
 
