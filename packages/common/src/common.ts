ef1a820aa580352445b57c0384c34e09ec5f5f94
Sean Chen <kamiyo@gmail.com>
2023-11-14 21:27:05 -0600

    fix(common): some linting, typings, and failure to call function

--- packages/common/src/common.ts
+++ packages/common/src/common.ts
@@ -1,7 +1,8 @@
 import { differenceInCalendarYears } from 'date-fns';
 
 export const baseString = 'Sean Chen: Pianist, Composer, Arranger | ';
-export const getAge = () => differenceInCalendarYears(new Date(), new Date(1988, 7, 27));
+export const getAge = () =>
+    differenceInCalendarYears(new Date(), new Date(1988, 7, 27));
 export const descriptions: {
     home: string;
     biography: string;
@@ -19,23 +20,24 @@ export const descriptions: {
     getMusic: (piece: string, contributors?: string) => string;
     searchResults: (query: string) => string;
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-    [key: string]: any;
+    [key: string]: string | Function;
 } = {
-    home: `Welcome to the official website of pianist, composer, and arranger Sean Chen. Third Prize at the 2013 Van Cliburn, Christel DeHaan Classical Fellow of the 2013 American Pianists Awards, and Artist-in-Residence at University of Missouri, Kansas City.`,
-    biography: `Hailed as a charismatic rising star with “an exceptional ability to connect with an audience combined with an easy virtuosity” (Huffington Post), ${getAge}-year-old American pianist Sean Chen, third prize winner at the 2013 Van Cliburn International Piano Competition and recipient of the DeHaan Classical Fellowship as the winner of the 2013 American Pianists Awards, has continued to earn accolades for “alluring, colorfully shaded renditions” (New York Times) and “genuinely sensitive” (LA Times) playing.`,
+    home: 'Welcome to the official website of pianist, composer, and arranger Sean Chen. Third Prize at the 2013 Van Cliburn, Christel DeHaan Classical Fellow of the 2013 American Pianists Awards, and Artist-in-Residence at University of Missouri, Kansas City.',
+    biography: `Hailed as a charismatic rising star with “an exceptional ability to connect with an audience combined with an easy virtuosity” (Huffington Post), ${getAge()}-year-old American pianist Sean Chen, third prize winner at the 2013 Van Cliburn International Piano Competition and recipient of the DeHaan Classical Fellowship as the winner of the 2013 American Pianists Awards, has continued to earn accolades for “alluring, colorfully shaded renditions” (New York Times) and “genuinely sensitive” (LA Times) playing.`,
     discography: 'Complete discography of Sean Chen',
-    contact: `Contact information for Sean Chen and for booking performances.`,
+    contact: 'Contact information for Sean Chen and for booking performances.',
     upcoming: 'Upcoming recitals, concerti, and masterclasses.',
     archive: 'Past recitals, concerti, and masterclasses.',
     search: 'Search recitals, concerti, and masterclasses,',
-    searchResults: (query: string) =>
-        `Search results for "${query}"`,
+    searchResults: (query: string) => `Search results for "${query}"`,
     videos: `A playlist of Sean Chen's YouTube clips.`,
     music: `A playlist of Sean Chen's live concert recordings, and a link to his Spotify musician page.`,
     getMusic: (piece: string, contributors?: string) =>
-        `Listen to Sean Chen's live performance of ${piece}` + (contributors ? `, with ${contributors}` : '.'),
+        `Listen to Sean Chen's live performance of ${piece}${
+            contributors ? `, with ${contributors}` : '.'
+        }`,
     photos: 'Publicity photos for browsing, and a link to a Dropbox folder for high-resolution images.',
     press: `Reviews of Sean Chen's performances.`,
     scores: `Online shop of Sean Chen's arrangements, cadenzas, and original compositions.`,
-    faqs: `Information about Sean Chen Piano online shop.`,
+    faqs: 'Information about Sean Chen Piano online shop.',
 };
